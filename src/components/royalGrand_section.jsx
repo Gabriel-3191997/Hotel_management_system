90fdd92fa5e266c18dcc7459f6d814f8b96a34a6
Gabriel_31997 <gabrielwkun@gmail.com>
2026-04-01 15:46:49 +0000

    update

--- src/components/royalGrand_section.jsx
+++ src/components/royalGrand_section.jsx
@@ -1,26 +1,31 @@
 import React from "react";
-import img1 from "../assets/images/Z (10).jpeg";
-class RoyalGrandSection extends React.Components {
+import img1 from "../assets/images/Z (6).jpeg";
+class RoyalGrandSection extends React.Component {
   render() {
     return (
       <>
-        <div className="flex flex-wrap justify-between bg-white md:gap-10 h-auto md:mt-28 md:mx-20  mt-2">
+        <div className="flex flex-wrap justify-evenly bg-white md:gap-10 h-auto md:mt-28 md:mx-20  mt-2">
           <div className="md:w-lg mx-2 mt-14 md:mt-0 bg-white">
             <img
               src={img1}
               alt="royal grand hotel"
-              className="h-96 w-auto object-cover rounded-sm"
+              className="md:h-96 md:w-auto h-auto w-xl object-cover rounded-sm"
             />
+            <div className="flex flex-wrap justify-start md:my-10">
+              <div className="w-20">1</div>
+              <div className="w-20">2</div>
+              <div className="w-20">3</div>
+            </div>
           </div>
           <div className="w-lg bg-white">
-            <div className="border-none md:border-gray-500 h-96 w-96 md:shadow-sm bg-white">
-              <h1 className="text-center font-sans text-2xl capitalize font-semibold md:py-10">
+            <div className="border-none mx-5 py-8 md:border-gray-500 h-96 w-96  md:shadow-none bg-white">
+              <h1 className="text-left font-sans text-2xl capitalize font-semibold md:py-10">
                 royal grand hotel
               </h1>
-              <p className="text-center font-sans py-2 text-lg capitalize">
+              <p className="text-left font-sans py-8 text-lg capitalize">
                 15 street sinkor <br /> monrovia, liberia
               </p>
-              <div className=" md:mt-20 flex flex-wrap justify-center">
+              <div className=" md:mt-8 my-2 flex flex-wrap justify-start">
                 <a
                   href="#"
                   className="px-8 py-3 font-semibold text-lg uppercase rounded-sm bg-amber-500"
