b623410ac38c0b77186b1347bc0afea3cb4dd58c
Gabriel_31997 <gabrielwkun@gmail.com>
2026-04-14 16:14:09 +0000

    database connected

--- server/app.js
+++ server/app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mysql = require("mysql");
 const port = 8080;
 const app = express();
 
+// connect to database
+
+const con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password@123",
+});
+
 app
   .get("/home", (req, res) => {
-    res.send("hello,world!");
+    res.send("hello,world");
+
+    con.connect(function (err) {
+      if (err) console.log("Connected!");
+    });
   })
+
   .listen(port, (err) => {
     if (!err) {
       console.log(`server is running on port ${port}`);
