d7e393d4b32d39f64608b94f16fe7f4405515d5d
Gabriel_31997 <gabrielwkun@gmail.com>
2026-04-14 16:38:33 +0000

    tables created

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const con = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "password@123",
+  database: "hotels",
 });
 
 app
@@ -17,6 +18,18 @@ app
 
     con.connect(function (err) {
       if (err) console.log("Connected!");
+
+      // const sql =
+      //   "CREATE TABLE IF NOT EXISTS user_credentials (username VARCHAR(255),email VARCHAR(255),password VARCHAR(255))";
+      // const sql =
+      //   "CREATE TABLE IF NOT EXISTS hotels ( Hotel VARCHAR(255), Location VARCHAR(255), Email VARCHAR(255), Phone VARCHAR(255), Price VARCHAR(255))";
+
+      const sql =
+        "CREATE TABLE IF NOT EXISTS bookings  ( Customers VARCHAR(255), Email VARCHAR(255), Hotel VARCHAR(255), Availability VARCHAR(255), Departure VARCHAR(255), Rooms VARCHAR(255), Suites VARCHAR(255), Adult VARCHAR(255), Children VARCHAR(255), Payment VARCHAR(255))";
+
+      con.query(sql, function (err) {
+        if (err) console.log("table created");
+      });
     });
   })
 
